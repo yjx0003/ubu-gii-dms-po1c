00b2e4607ed59079a3693aacab71fa3a6bcdd6b6
Yi Peng <yjx0003@alu.ubu.es>
2018-11-12 21:06:14 +0100

    Cambio a protected el atributo tareas de Backlog

--- Kanban/src/model/Backlog.java
+++ Kanban/src/model/Backlog.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Backlog {
-	private List<Tarea> tareas;
+	protected List<Tarea> tareas;
 	
 	public Backlog(){
 		tareas = new ArrayList<Tarea>(); 
